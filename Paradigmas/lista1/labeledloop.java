7d027ffe78cdb94bdf7fdb4222f5413db63cd6b9
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-15 23:07:17 -0300

    bla

--- Paradigmas/lista1/labeledloop.java
+++ Paradigmas/lista1/labeledloop.java
@@ -1,11 +1,13 @@
+import java.util.Random;
 public class labeledloop {
   public static void main(String[] args) {
+    Random generate = new Random();
     outer:
     for (int i = 0; i < 10; i++) {
       System.out.println("Loop de fora: " + i);
       for (int j = 0; j < 10; j++) {
         System.out.println("Loop de dentro: " + j);
-        if(i==7 && j==7) 
+        if(i==generate.nextInt(10) && j==generate.nextInt(10))  
           break outer;
      } 
     }
