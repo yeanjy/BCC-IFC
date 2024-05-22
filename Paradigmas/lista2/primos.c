ac51eeddaed8d030697f21c5258bf70d1dccc05c
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-22 13:13:39 -0300

    bla

---
+++ Paradigmas/lista2/primos.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int isPrime(int n)
+{
+  if (n <= 1)
+    return 0;
+
+  for(int i = 2; i <= n/2; i++)
+  {
+    if (n % i == 0)
+      return 0;
+  }
+  return 1;
+}
+
+void drawPrimes(int arr[], int size)
+{
+  srand(time(0)); 
+  int count = 0;
+
+outerloop:
+  while (count < size)
+  {
+    int r = rand()%101;
+    if (isPrime(r))
+    { 
+      for(int i = 0; i < count; i++)
+      {
+        if (r == arr[i])
+          goto outerloop;
+      }
+
+      arr[count] = r;
+      count++;
+    }
+  }
+}
+
+void printArray(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d\n", arr[i]);
+  }
+}
+
+int main()
+{
+  int arr[25]; 
+  drawPrimes(arr, 25);
+  printArray(arr, 25);
+
+  return 0;
+}
