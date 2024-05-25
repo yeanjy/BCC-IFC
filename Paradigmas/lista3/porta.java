9c68da2d56beba4ee71aa9c7db29a78f70731db6
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-25 00:29:37 -0300

    bla

---
+++ Paradigmas/lista3/porta.java
@@ -0,0 +1,19 @@
+public class porta {
+  private boolean aberta;
+  private String cor;
+  private double dimensaoX, dimensaoY, dimensaoZ;
+
+  porta(boolean aberta, String cor, double dimensaoX, double dimensaoY, double dimensaoZ)
+  {
+    this.aberta = aberta;
+    this.cor = cor;
+    this.dimensaoX = dimensaoX;
+    this.dimensaoY = dimensaoY;
+    this.dimensaoZ = dimensaoZ;
+  }
+
+  public void abre() {this.aberta = true;}
+  public void fecha() {this.aberta = false;}
+  public void pinta (String cor) {this.cor = cor;}
+  public boolean estaAberta() {return this.aberta;}
+}
