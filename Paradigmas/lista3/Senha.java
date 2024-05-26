5fb43f8c18b4a984f44661189e3a2e9e046cf875
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-25 22:18:08 -0300

    commit

--- Paradigmas/lista3/Senha.java
+++ Paradigmas/lista3/Senha.java
@@ -7,6 +7,27 @@ public class Senha {
         this.tentativa = tentativa;
     }
 
+    public void trocarSenha(String senhaAntiga, String senhaNova)
+    {
+        if (this.tentativa >= 3)
+        {
+            System.out.println("Senha bloqueado");
+            return;
+        }
+
+        if (this.senha.equals(senhaAntiga))
+        {
+            this.tentativa = 0;
+            this.senha = senhaNova;
+            System.out.println("Senha alterado com sucesso");
+        }
+        else
+        {
+            this.tentativa++;
+            System.out.println("Senha incorreta, ainda possui " + this.tentativa + " tentativas.");
+        }
+    }
+
    public void entraSenha(String senha)
    {
        if (this.tentativa >= 3)
