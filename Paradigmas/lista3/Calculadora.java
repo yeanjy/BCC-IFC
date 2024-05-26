c0051469c5803c6e188d18d9c2180d5f9af970fd
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-25 23:58:28 -0300

    commmit

---
+++ Paradigmas/lista3/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+    public double multiplicar(double x, double y)
+    {
+        return x*y;
+    }
+
+    public double dividir(double x, double y)
+    {
+        return x/y;
+    }
+
+    public double somar (double x, double y)
+    {
+        return x+y;
+    }
+
+    public double subtrair (double x, double y)
+    {
+        return x-y;
+    }
+}
---
+++ Paradigmas/lista3/CalculadoraCientifica.java
@@ -0,0 +1,6 @@
+public class CalculadoraCientifica extends Calculadora{
+    double potencia(double x, double y)
+    {
+        return Math.pow(x, y);
+    }
+}
