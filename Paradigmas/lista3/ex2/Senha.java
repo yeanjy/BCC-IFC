c6ad91486b1435b220b42ebae41d7fba3dcfc1ca
Yean Jy Chen <yeanchen3@gmail.com>
2024-05-30 18:32:44 -0300

    commit

--- Paradigmas/lista3/ex2/Senha.java
+++ Paradigmas/lista3/ex2/Senha.java
@@ -1,52 +1,107 @@
 package ex2;
 
+import java.util.Scanner;
+
 public class Senha {
     private String senha;
     private int tentativa;
 
-    public Senha(String senha, int tentativa) {
+    public Senha(String senha) {
         this.senha = senha;
-        this.tentativa = tentativa;
+        this.tentativa = 1;
     }
 
-    public void trocarSenha(String senhaAntiga, String senhaNova)
-    {
-        if (this.tentativa >= 3)
-        {
-            System.out.println("Senha bloqueado");
+    public void trocarSenha(String senhaAntiga, String senhaNova) {
+        if (this.tentativa > 3) {
+            System.out.println("Senha bloqueada");
             return;
         }
 
-        if (this.senha.equals(senhaAntiga))
-        {
-            this.tentativa = 0;
+        if (this.senha.equals(senhaAntiga)) {
+            this.tentativa = 1;
             this.senha = senhaNova;
-            System.out.println("Senha alterado com sucesso");
+            System.out.println("Senha alterada com sucesso");
         }
-        else
-        {
+        else {
             this.tentativa++;
-            System.out.println("Senha incorreta, ainda possui " + (3 - this.tentativa) + " tentativas.");
+            if (this.tentativa > 3) {
+                System.out.println("Senha bloqueada");
+            } else {
+                System.out.println("Senha incorreta, ainda possui " + (4 - this.tentativa) + " tentativas.");
+            }
         }
     }
 
-   public void entraSenha(String senha)
-   {
-       if (this.tentativa >= 3)
-       {
-           System.out.println("Senha bloqueado");
-           return;
-       }
+    public void entraSenha(String senha) {
+        if (this.tentativa > 3) {
+            System.out.println("Senha bloqueada");
+            return;
+        }
 
-       if (senha.equals(this.senha))
-       {
-           System.out.println("Senha correta");
-           this.tentativa = 0;
-       }
-       else
-       {
-           ++tentativa;
-           System.out.println("Senha incorreta, ainda possui " + (3 - this.tentativa) + " tentativas.");
-       }
-   }
+        if (senha.equals(this.senha)) {
+            System.out.println("Senha correta");
+            this.tentativa = 1;
+        }
+        else {
+            this.tentativa++;
+            if (this.tentativa > 3) {
+                System.out.println("Senha bloqueada");
+            } else {
+                System.out.println("Senha incorreta, ainda possui " + (4 - this.tentativa) + " tentativas.");
+            }
+        }
+    }
+
+    void run() {
+        Scanner in = new Scanner(System.in);
+        boolean isRunning = true;
+        while (isRunning) {
+            if (this.tentativa > 3) {
+                return;
+            }
+
+            System.out.println("Opções: ");
+            System.out.println("1 - Entrar");
+            System.out.println("2 - Trocar senha");
+            System.out.println("3 - Sair");
+            String entradaStr = in.nextLine();
+            int entrada;
+            try {
+                entrada = Integer.parseInt(entradaStr);
+            } catch (NumberFormatException e) {
+                System.out.println("Opção inválida");
+                continue;
+            }
+
+            switch (entrada) {
+                case 1:
+                    System.out.println("Digite a sua senha: ");
+                    String senha = in.nextLine();
+                    entraSenha(senha);
+                    break;
+
+                case 2:
+                    System.out.println("Digite a sua senha antiga: ");
+                    String senhaAntiga = in.nextLine();
+                    System.out.println("Digite a sua senha nova: ");
+                    String senhaNova = in.nextLine();
+                    trocarSenha(senhaAntiga, senhaNova);
+                    break;
+
+                case 3:
+                    System.out.println("Saindo do sistema");
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+        in.close();
+    }
+
+    public static void main(String[] args) {
+        Senha minhaSenha = new Senha("bla");
+        minhaSenha.run();
+    }
 }
--- Paradigmas/lista3/ex5/TestarCalculadora.java
+++ Paradigmas/lista3/ex5/TestarCalculadora.java
@@ -2,6 +2,13 @@ package ex5;
 
 public class TestarCalculadora {
     public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        CalculadoraCientifica calccien = new CalculadoraCientifica();
 
+        System.out.println("Resultado de somar(2, 3): " + calc.somar(2, 3));
+        System.out.println("Resultado de dividir(0, 0): " + calc.dividir(0, 0));
+        System.out.println("Resultado de multiplicar(13, 22): " + calc.multiplicar(13, 22));
+        System.out.println("Resultado de subtrair(11, 22): " + calc.subtrair(11, 22));
+        System.out.println("Resultado de potencia(0, 0): " + calccien.potencia(0, 0));
     }
 }
