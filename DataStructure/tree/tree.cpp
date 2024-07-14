e6f7d057c937bf24cb62aae388ebeaba5205f560
Yean Jy Chen <yeanchen3@gmail.com>
2024-07-14 10:41:21 -0300

    commit

--- DataStructure/tree/tree.cpp
+++ DataStructure/tree/tree.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 
-struct node
-{
+struct tree {
+private:
+  struct node {
     node *l;
     node *r;
     int val;
 
-    node(int val) : val(val), l(nullptr), r(nullptr)
-    {
-    }
-};
+    node(int val) : val(val), l(nullptr), r(nullptr) {}
+  };
 
-struct binaryTree
-{
-    node *root;
+  node *root;
 
-    binaryTree() : root(nullptr)
-    {
+  void insertHelper(node *current, int val) {
+    if (current->val < val) {
+      if (!current->l) {
+        current->l = new node(val);
+      } else {
+        insertHelper(current->l, val);
+      }
+    } else {
+      if (!current->r) {
+        current->r = new node(val);
+      } else {
+        insertHelper(current->r, val);
+      }
     }
+  }
 
-    void insertion(int val)
-    {
-        if (!root)
-        {
-            root = new node(val);
-        }
-        else
-        {
-        }
+  node *searchHelper(node *current, int val) {
+    if (!current || current->val == val)
+      return current;
+
+    if (current->val < val) {
+      return searchHelper(current->l, val);
+    } else {
+      return searchHelper(current->r, val);
     }
+  }
+
+public:
+  tree() : root(nullptr) {}
+
+  void insertion(int val) {
+    if (!root) {
+      root = new node(val);
+    } else {
+      insertHelper(root, val);
+    }
+  }
+
+  node *search(int val) { return searchHelper(root, val); }
 };
