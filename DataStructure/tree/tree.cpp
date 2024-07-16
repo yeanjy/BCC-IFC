c5bd45de02af23e9d75d27c585a82cf5b9106fea
Yean Jy Chen <yeanchen3@gmail.com>
2024-07-16 10:05:17 -0300

    commit

--- DataStructure/tree/tree.cpp
+++ DataStructure/tree/tree.cpp
@@ -13,27 +13,97 @@ private:
 
   node *root;
 
-  node *searchHelper(node *current, int key) {
-    if (!current || current->key == key)
-      return current;
-
-    if (current->key < key) {
-      return searchHelper(current->l, key);
-    } else {
-      return searchHelper(current->r, key);
-    }
+  int max (int a, int b) {
+    return a > b? a:b;
   }
+
+  node *rightRotate(node *n) {
+    node *aux = n->l;
+    node *aux2 = aux->r;
+
+    aux->r = n;
+    n->l = aux2;
+    updateHeight(n);
+    updateHeight(aux);
+    return aux;
+}
+
+node *leftRotate (node *n) {
+  node *aux = n->r;
+  node *aux2 = aux->l;
+
+  aux->l = n;
+  n->r = aux2;
+  updateHeight(n);
+  updateHeight(aux);
+  return aux;
+}
+
+int height (node *n) {
+  return n? n->height : 0;
+}
+
+int balanceFactor (node *n) {
+  return n? height(n->l) - height(n->r) : 0;
+}
+
+void updateHeight (node *n) {
+  n->height = 1 + max(height(n->l), height(n->r));
+}
+
+node *balance(node *n) {
+  updateHeight(n);
+
+  int balance = balanceFactor(n);
+
+  if (balance > 1) {
+    if (balanceFactor(n->l) < 0) {
+      n->l = leftRotate(n->l);
+    }
+    return rightRotate(n);
+  }
+  if (balance < -1) {
+    if (balanceFactor(n->r) > 0) {
+      n->r = rightRotate(n->r);
+    }
+    return leftRotate(n);
+  }
+
+  return n;
+}
+
+node *insert(node *n, int key) {
+  if (!n)
+    return new node (key);
+
+  if (key < n->key)
+    n->l = insert (n->l, key);
+  else if (key > n->key)
+    n->r = insert(n->r, key);
+  else 
+    return n;
+
+  return balance(n);
+}
 
 public:
   tree() : root(nullptr) {}
-
-  int height (node *n) {
-    return n? n->height : 0;
+  
+  void insert (int key) {
+    insert(root, key);
   }
 
-  int balanceFactor (node *n) {
-    return n? height(n->l) - height(n->r) : 0;
-  }
-
-  node *search(int key) { return searchHelper(root, key); }
 };
+
+int main() {
+  tree t;
+  t.insert(4);
+  t.insert(1);
+  t.insert(3);
+  t.insert(443);
+  t.insert(12312);
+  t.insert(95342);
+  t.insert(312);
+
+  return 0;
+}
