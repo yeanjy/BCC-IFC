4c6e492baa40629715e9d2fec9b2a678d7269f21
Yean Jy Chen <yeanchen3@gmail.com>
2024-07-20 14:14:23 -0300

    commit

--- DataStructure/tree/tree.cpp
+++ DataStructure/tree/tree.cpp
@@ -26,6 +26,31 @@ private:
     return n;
   }
 
+  node *insertHelperLoop(node *n, int val) {
+    if (!n)
+      return new node(val);
+
+    node *current = n;
+    node *parent;
+
+    while (current) {
+      parent = current;
+      if (val < current->key)
+        current = current->left;
+      else if (val > current->key)
+        current = current->right;
+      else
+        return n;
+    }
+
+    if (val < parent->key)
+      parent->left = new node(val);
+    else
+      parent->right = new node(val);
+
+    return n;
+  }
+
   node *searchRec(node *n, int val) {
     if (!n)
       return nullptr;
@@ -39,10 +64,33 @@ private:
       return searchRec(n->left, val);
   }
 
+  node *search(node *n, int val) {
+    node *aux = n;
+    while (aux) {
+      if (aux->key == val)
+        return aux;
+      if (val > aux->key) {
+        aux = aux->right;
+      } else
+        aux = aux->left;
+    }
+    return aux;
+  }
+
+  void printInOrderHelper(node *n) {
+    if (n) {
+      printInOrderHelper(n->left);
+      std::cout << n->key << std::endl;
+      printInOrderHelper(n->right);
+    }
+  }
+
 public:
   tree() : root(nullptr) {};
 
   void insertRec(int val) { root = insertHelper(root, val); }
+  void insert(int val) { root = insertHelperLoop(root, val); }
+  void printInOrder() { printInOrderHelper(root); }
 };
 
 int main() {
@@ -55,5 +103,11 @@ int main() {
   t.insertRec(4);
   t.insertRec(6);
   t.insertRec(8);
+  t.insert(19);
+  t.insert(29);
+  t.insert(41);
+  t.insert(39);
+  t.printInOrder();
+
   return 0;
 }
