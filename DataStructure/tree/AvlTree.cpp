99ff21bb41dc974f6553b56c68aae0edad13077c
Yean Jy Chen <yeanchen3@gmail.com>
2024-07-20 11:23:04 -0300

    commit

--- DataStructure/tree/AvlTree.cpp
+++ DataStructure/tree/AvlTree.cpp
@@ -1,121 +1,110 @@
 #include <iostream>
 
 struct node {
-    int key;
-    node *left;
-    node *right;
-    int height;
+  int key;
+  node *left;
+  node *right;
+  int height;
 
-    node(int key)
-    :key(key), left(nullptr), right(nullptr), height(0) {}
+  node(int key) : key(key), left(nullptr), right(nullptr), height(0) {}
 };
 
 class AvlTree {
-private: 
-    node *root;
+private:
+  node *root;
 
-    int max(int a, int b) {
-        return a > b? a:b;
+  int max(int a, int b) { return a > b ? a : b; }
+
+  int height(node *n) { return n ? n->height : -1; }
+
+  int balanceFactor(node *n) {
+    return n ? height(n->left) - height(n->right) : 0;
+  }
+
+  void updateHeight(node *n) {
+    n->height = max(height(n->left), height(n->right)) + 1;
+  }
+
+  node *rightRotate(node *n) {
+    node *child = n->left;
+    node *grandChild = child->right;
+
+    child->right = n;
+    n->left = grandChild;
+
+    updateHeight(n);
+    updateHeight(child);
+
+    return child;
+  }
+
+  node *leftRotate(node *n) {
+    node *child = n->right;
+    node *grandChild = child->left;
+
+    child->left = n;
+    n->right = grandChild;
+
+    updateHeight(n);
+    updateHeight(child);
+
+    return child;
+  }
+
+  node *rotate(node *n) {
+    int _balanceFactor = balanceFactor(n);
+
+    if (_balanceFactor > 1) {
+      if (balanceFactor(n->left) >= 0) {
+        return rightRotate(n);
+      } else {
+        n->left = leftRotate(n->left);
+        return rightRotate(n);
+      }
     }
 
-    int height(node *n){
-        return n ? n->height : -1;
+    if (_balanceFactor < -1) {
+      if (balanceFactor(n->right) <= 0) {
+        return leftRotate(n);
+      } else {
+        n->right = rightRotate(n->right);
+        return leftRotate(n);
+      }
     }
+    return n;
+  }
 
-    int balanceFactor(node *n) {
-        return n ? height(n->left) - height(n->right) : 0;
-    }
+  node *insertHelper(node *n, int val) {
+    if (!n)
+      return new node(val);
 
-    void updateHeight(node *n) {
-        n->height = max(height(n->left), height(n->right)) + 1;
-    }
+    if (val < n->key)
+      n->left = insertHelper(n->left, val);
+    else if (val > n->key)
+      n->right = insertHelper(n->right, val);
+    else
+      return n;
 
-    node *rightRotate(node *n) {
-        node *child = n->left;
-        node *grandChild = child->right;
+    updateHeight(n);
+    n = rotate(n);
 
-        child->right = n;
-        n->left = grandChild;
-
-        updateHeight(n);
-        updateHeight(child);
-
-        return child;
-    }
-
-    node *leftRotate(node *n) {
-        node *child = n->right;
-        node *grandChild = child->left;
-
-        child->left = n;
-        n->right = grandChild;
-
-        updateHeight(n);
-        updateHeight(child);
-
-        return child;
-    }
-
-    node *rotate(node *n) {
-        int _balanceFactor = balanceFactor(n);
-
-        if(_balanceFactor > 1) {
-            if (balanceFactor(n->left) >= 0) {
-                return rightRotate(n);
-            }
-            else {
-                n->left = leftRotate(n->left);
-                return rightRotate(n);
-            }
-        }
-
-        if (_balanceFactor < -1) {
-            if(balanceFactor(n->right) <= 0) {
-                return leftRotate(n);
-            }
-            else {
-                n->right = rightRotate(n->right);
-                return leftRotate(n);
-            }
-        }
-        return n;
-    }
-
-    node *insertHelper(node *n, int val) {
-        if (!n)
-            return new node(val);
-
-        if (val < n->key)
-            n->left = insertHelper(n->left, val);
-        else if (val > n->key)
-            n->right = insertHelper(n->right, val);
-        else 
-            return n;
-
-        updateHeight(n);
-        n = rotate(n);
-
-        return n;
-    }
+    return n;
+  }
 
 public:
-    AvlTree()
-    :root(nullptr) {}
+  AvlTree() : root(nullptr) {}
 
-    void insert(int val) {
-        root = insertHelper(root, val);
-    }
-
+  void insert(int val) { root = insertHelper(root, val); }
 };
 
 int main() {
-    AvlTree t;
-    t.insert(4);
-    t.insert(1);
-    t.insert(3);
-    t.insert(443);
-    t.insert(12312);
-    t.insert(95342);
-    t.insert(312);
-    return 0;
+  AvlTree t;
+  t.insert(4);
+  t.insert(1);
+  t.insert(3);
+  t.insert(443);
+  t.insert(12312);
+  t.insert(95342);
+  t.insert(312);
+  return 0;
 }
--- DataStructure/tree/tree.cpp
+++ DataStructure/tree/tree.cpp
@@ -1,109 +1,48 @@
 #include <iostream>
 
-class tree {
-private:
-  struct node {
-    node *l;
-    node *r;
-    int key;
-    int height;
+struct node {
+  int key;
+  node *left;
+  node *right;
 
-    node(int key) : key(key), l(nullptr), r(nullptr), height(0) {}
-  };
+  node(int val) : key(val), left(nullptr), right(nullptr) {}
 
-  node *root;
-
-  int max (int a, int b) {
-    return a > b? a:b;
-  }
-
-  node *rightRotate(node *n) {
-    node *aux = n->l;
-    node *aux2 = aux->r;
-
-    aux->r = n;
-    n->l = aux2;
-    updateHeight(n);
-    updateHeight(aux);
-    return aux;
-}
-
-node *leftRotate (node *n) {
-  node *aux = n->r;
-  node *aux2 = aux->l;
-
-  aux->l = n;
-  n->r = aux2;
-  updateHeight(n);
-  updateHeight(aux);
-  return aux;
-}
-
-int height (node *n) {
-  return n? n->height : -1;
-}
-
-int balanceFactor (node *n) {
-  return n? height(n->l) - height(n->r) : 0;
-}
-
-void updateHeight (node *n) {
-  n->height = 1 + max(height(n->l), height(n->r));
-}
-
-node *balance(node *n) {
-  updateHeight(n);
-
-  int balance = balanceFactor(n);
-
-  if (balance > 1) {
-    if (balanceFactor(n->l) < 0) {
-      n->l = leftRotate(n->l);
-    }
-    return rightRotate(n);
-  }
-  if (balance < -1) {
-    if (balanceFactor(n->r) > 0) {
-      n->r = rightRotate(n->r);
-    }
-    return leftRotate(n);
-  }
-
-  return n;
-}
-
-node *insert(node *n, int key) {
-  if (!n)
-    return new node (key);
-
-  if (key < n->key)
-    n->l = insert (n->l, key);
-  else if (key > n->key)
-    n->r = insert(n->r, key);
-  else 
-    return n;
-
-  return balance(n);
-}
-
-public:
-  tree() : root(nullptr) {}
-  
-  void insert (int key) {
-    insert(root, key);
-  }
-
+  node(int val, node *left, node *right) : key(val), left(left), right(right) {}
 };
 
-int main() {
-  tree t;
-  t.insert(4);
-  t.insert(1);
-  t.insert(3);
-  t.insert(443);
-  t.insert(12312);
-  t.insert(95342);
-  t.insert(312);
+struct tree {
+private:
+  node *root;
 
-  return 0;
-}
+  node *insertHelper(node *n, int val) {
+    if (!n)
+      return new node(val);
+
+    if (val > n->key)
+      n->right = insertHelper(n->right, val);
+    else if (val < n->key)
+      n->left = insertHelper(n->left, val);
+
+    return n;
+  }
+
+  node *searchRec(node *n, int val) {
+    if (!n)
+      return nullptr;
+
+    if (val == n->key)
+      return n;
+
+    if (val > n->key)
+      return searchRec(n->right, val);
+    else
+      return searchRec(n->left, val);
+  }
+
+public:
+  tree() : root(nullptr) {};
+
+  void insertRec(int val) { root = insertHelper(root, val); }
+};
+
+int main() { return 0; }
